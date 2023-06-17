c238a5b3058c88f041dfe8c8302d4fedcff728bf
Pluxuryworld <pluxuryworld@gmail.com>
2023-06-17 14:18:50 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'qrcode-generator';
+  title = 'qr-generator';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ColorComponent } from './components/color/color.component';
+import { ColorsComponent } from './components/colors/colors.component';
+import { SizeComponent } from './components/size/size.component';
+import { QrComponent } from './components/qr/qr.component';
+import { TextComponent } from './components/text/text.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { DownloadComponent } from './components/download/download.component';
+import { ShareComponent } from './components/share/share.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorComponent,
+    ColorsComponent,
+    SizeComponent,
+    QrComponent,
+    TextComponent,
+    ButtonsComponent,
+    DownloadComponent,
+    ShareComponent
   ],
   imports: [
     BrowserModule,
